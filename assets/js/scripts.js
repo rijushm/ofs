ea27e66038aa1151886fc85f40698fae6b35b811
rijushm <rijushm@gmail.com>
2022-01-22 11:28:09 +0530

    Initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,38 @@
+AOS.init();
+$(window).on('load', function(){
+	$('.pllax').paroller();
+	var dropmob = $(".dropdown-mobile").height();
+	var headerheight = $("header").height();
+	var winheight = $(window).height();
+	$(".dropdown-mobile").height(0);
+	$(".menu").css('top', headerheight);
+	$(".menu").height(winheight-headerheight);
+	$("#hamburger").click(function(){
+		$(this).toggleClass('open');
+		$('.menu').toggleClass('active');
+		if ($('.menu').hasClass('enabled')) {
+			$('.menu').removeClass('enabled');
+		}else{
+			setTimeout(function(){
+				$('.menu').addClass('enabled');
+			}, 1800)
+		}
+		setTimeout(function(){
+			$('.menu li').toggleClass('active');
+		}, 700)
+	})
+	$(".dropdown-menu-mobile").hover(()=>{
+		if ($(".dropdown-mobile").height() == 0) {
+			$(".dropdown-mobile").height(dropmob);
+		}else{
+			$(".dropdown-mobile").height(0);
+		}
+	})
+	$(".dropdown-menu-mobile").click(()=>{
+		if ($(".dropdown-mobile").height() == 0) {
+			$(".dropdown-mobile").height(dropmob);
+		}else{
+			$(".dropdown-mobile").height(0);
+		}
+	})
+})
